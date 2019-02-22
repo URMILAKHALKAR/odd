931f25ee812aa0426d4a926579b63d741b76f039
URMILAKHALKAR <47715304+URMILAKHALKAR@users.noreply.github.com>
2019-02-22 13:58:16 +0530

    logic for odd no is written

---
+++ odd_no.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("Enter no :");
+ scanf("%d",&n);
+   if((n%2)==0)
+   {
+   printf("%d is even no",n);
+   }
+   else
+   {
+   printf("%d is odd no",n);
+   }
+}
